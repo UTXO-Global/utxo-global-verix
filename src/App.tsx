21e33b66b7e91222292165837013f04b7a4cddc8
KhoiNguyen <nguyen.nguyen@apps-cyclone.com>
2025-01-07 15:16:02 +0700

    feat(connect): connect wallet

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from "@/components/ui/toaster";
 import VeriXForm from "@/pages/VeriXForm";
 
 const App = () => {
   return (
     <div className="bg-slate-200 min-h-screen flex items-center justify-center">
       <VeriXForm />
+      <Toaster />
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import CCCProvider from "@/providers/ccc-provider.tsx";
+import { AppProvider } from "@/providers/app-provider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <CCCProvider>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </CCCProvider>
-  </StrictMode>,
+  </StrictMode>
 );
